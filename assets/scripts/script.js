21749357057fa7a64868002377f09587f061393f
Pyrad-Time <macspyrad@gmail.com>
2026-04-09 10:23:25 -0300

    feat(calculator): implement basic operation logic with state management

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,9 +4,65 @@ const array = [...buttons]
 
 
 
+let firstNumber = null
+let operator = null
+let secondNumber = null
+
+function add (a, b) {
+    return console.log(a + b)
+}
+function subtract (a, b) {
+    return console.log(a - b)
+}
+function multiply (a, b) {
+    return console.log(a * b)
+}
+function divide (a, b) {
+    return console.log(a / b)
+}   
+
+
 array.forEach(button => {
     button.addEventListener('click', (e) => {
-        const buttonItem = e.target
-        console.log(buttonItem.dataset)
+        const buttonItem = e.currentTarget
+        const dataAction = buttonItem.dataset.action
+        const dataNumber = buttonItem.dataset.number
+        const number = Number(dataNumber)
+        
+        if (dataNumber) {
+            if(!operator) {
+                firstNumber = number
+            } else {
+                secondNumber = number
+            }
+        }
+
+        if (dataAction === "operator--add") {
+            operator = "+"
+        }
+        
+        if (dataAction === "operator--subtract") {
+            operator = "-"
+        }
+        
+        if (dataAction === "operator--multiply") {
+            operator = "x"
+        }
+        
+        if (dataAction === "operator--divide") {
+            operator = "/"
+        }
+
+
+        if(dataAction === "operator--equal") {
+            switch(operator) {
+                case "+": 
+                const result = add(firstNumber, secondNumber)
+                break
+            }
+        }
+
+
     })
 });
+
